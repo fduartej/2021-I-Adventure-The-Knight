8bbd44a5a7c8f87c00e837d91bd7cb1ff148d188
Fred Duarte <gestion_atlassian@inteligogroup.com>
2021-04-10 13:34:08 -0500

    Agregando Animaciones y Plantas

--- Assets/Player/Script/PlayerController.cs
+++ Assets/Player/Script/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 15f;
+    public float moveSpeed = 20f;
+    public bool isGrounded = false;
+    public bool isAttacking = false;
+    public float jumpForce = 400f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,14 +18,16 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Attack();
         Movement();
+        Jump();
     }
 
     private void Movement(){
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {   
-            //Cuando se mueve
             gameObject.GetComponent<Animator>().SetBool("ismovement", true);
+            gameObject.GetComponent<Animator>().SetBool("isattack", false);
             Vector3 movement = new Vector3(
                  Input.GetAxis("Horizontal"),
                  0f, 0f);
@@ -41,8 +46,42 @@ public class PlayerController : MonoBehaviour
         {
             //Cuando esta idle
             gameObject.GetComponent<Animator>().SetBool("ismovement", false);
+           
         }
+    }
 
+    void Jump(){
+        
+        if(Input.GetKeyDown(KeyCode.UpArrow) && isGrounded){
+             gameObject.GetComponent<Animator>().SetBool("isjumping", true);
+             gameObject.GetComponent<Rigidbody2D>().AddForce(
+                 new Vector2(0f, jumpForce), ForceMode2D.Impulse
+             );
+        } 
+        if(!Input.GetKey(KeyCode.UpArrow)){
+            gameObject.GetComponent<Animator>().SetBool("isjumping", false);
+            
+        }
+ 
+    }
 
+    void Attack(){
+         if(Input.GetKeyDown(KeyCode.X)){
+             gameObject.GetComponent<Animator>().SetBool("isattack", true);
+         }
+     }
+
+    private void OnCollisionEnter2D(Collision2D otherObject) {
+        
+        if(otherObject.collider.tag == "Floor" ){
+            isGrounded = true; 
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D otherObject) {
+        
+        if(otherObject.collider.tag == "Floor" ){
+            isGrounded = false; 
+        }
     }
 }
